6e43be2ba2e8e8c8d866fcb22d3f9cfb2f3b8f09
Vasyl Moisiienko <vasylmoisiienko@gmail.com>
2023-01-31 21:05:19 +0200

    Add test_http(response)

--- tests/test_http.py
+++ tests/test_http.py
@@ -11,6 +11,14 @@ def test_first_request():
 @pytest.mark.http
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
-    print(f'Response Body is {r.json()}'.encode("utf-8"))
-    print(f'Response Status code is {r.status_code}')
-    print(f'Response Headers are {r.headers}')
+    body = r.json()
+    headers = r.headers
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert headers['Server'] == 'GitHub.com'
+
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/sergii_butenko')
+    assert r.status_code == 404
