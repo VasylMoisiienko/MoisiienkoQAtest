59316efa92f464be0bbf74c64a5b101bd396dfcb
Vasyl Moisiienko <vasylmoisiienko@gmail.com>
2023-02-03 13:51:13 +0200

    Repo_search tests add

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -6,3 +6,11 @@ class GitHub:
         r = requests.get(f'https://api.github.com/users/{username}')
         body = r.json()
         return body
+
+    def search_repo(self, name):
+        r = requests.get(
+            'https://api.github.com/search/repositories',
+            params={'q': name}
+        )
+        body = r.json()
+        return body
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -11,3 +11,22 @@ def test_user_exists(github_api):
 def test_user_not_exists(github_api):
     r = github_api.get_user('butrnkosergii')
     assert r['message'] == 'Not Found'
+
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('become-qa-auto')
+    assert r['total_count'] == 26
+    assert 'become-qa-auto' in r['items'][0]['name']
+
+
+@pytest.mark.api
+def test_repo_cannot_be_found(github_api):
+    r = github_api.search_repo('sergiibutenko-repo-non_exist')
+    assert r['total_count'] == 0
+
+
+@pytest.mark.api
+def test_repo_with_single_char_be_found(github_api):
+    r = github_api.search_repo('s')
+    assert r['total_count'] != 0
